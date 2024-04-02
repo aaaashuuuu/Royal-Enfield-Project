17cda69a47da73d4ec9ced00f0e13293d8025c0f
aaaashuuuu <ashutoshsapkal1@gmail.com>
2024-04-02 21:22:40 +0530

    Project Started

---
+++ app.js
@@ -0,0 +1,48 @@
+var tip=document.getElementById("Pinky")
+function a1(){
+    tip.src="./Images/motorcycles.jpg"
+}
+function a(){
+    tip.src="./Images/motorcycle 2.avif"
+}
+function b(){
+    tip.src="./Images/motorcycle 3.avif"
+}
+function c(){
+    tip.src="./Images/motorcycle 4.avif"
+}
+function d(){
+    tip.src="./Images/motorcycle 5.avif"
+}
+function e(){
+    tip.src="./Images/motorcycle 6.avif"
+}
+function f(){
+    tip.src="./Images/motorcycle 7.avif"
+}
+function g(){
+    tip.src="./Images/motorcycle 8.avif"
+}
+function h(){
+    tip.src="./Images/motorcycle 9.avif"
+}
+
+var tip1=document.getElementById("Ashu")
+function r2(){
+    tip1.src="./Images/Rides 2.webp" 
+}
+function r3(){
+    tip1.src="./Images/Rides .jpg"
+}
+function r4(){
+    tip1.src="./Images/Rides 5.avif"
+}
+function r5(){
+    tip1.src="./Images/Rides 6.avif"
+}
+function r6(){
+    tip1.src="./Images/Rides 7.avif"
+}
+function r7(){
+    tip1.src="./Images/Rides 8.avif"
+}
